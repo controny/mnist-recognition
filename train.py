127783f9a444ccdf9f67faf375143b7d6b0cb61f
Dimitri <791219648@qq.com>
2018-05-15 22:31:40 +0800

    perform well

--- train.py
+++ train.py
@@ -5,10 +5,13 @@ import network
 
 def train():
     training_data, validation_data, test_data = data_loader.load_all_data()
-    # layers = [784, 100, 100, 10]
-    layers = [784, 20, 10]
+    layers = [784, 100, 10]
     net = network.Network(layers)
-    net.optimize(training_data, validation_data, 30, 1, 100, 0.005)
+    net.optimize(training_data, validation_data, 30, 1, 10, 1.0)
+    # 测试
+    num_correct = net.accuracy(test_data)
+    accuracy = 1.0 * num_correct / len(test_data)
+    print('Final: %f (%d / %d)' % (accuracy, num_correct, len(test_data)))
 
 
 if __name__ == '__main__':
