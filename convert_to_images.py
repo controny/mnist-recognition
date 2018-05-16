bed0d71d68ba1765460307e63fdf0ffe41cbf283
Dimitri <791219648@qq.com>
2018-05-16 22:17:59 +0800

    finish inference module

--- convert_to_images.py
+++ convert_to_images.py
@@ -1,21 +1,23 @@
 # coding=utf-8
 import data_loader
-import matplotlib.pyplot as plt
+from PIL import Image
 import numpy as np
 import os
 
 
 def convert():
+    """用于把MNIST的数据转为图片文件"""
     images_dir = 'images/'
     if not os.path.exists(images_dir):
         os.mkdir(images_dir)
     num_images = 10
-    images = data_loader.load_images(data_loader.test_images_path, num_images, binaryzation=True)
+    images = data_loader.load_images(data_loader.test_images_path, num_images, binaryzation=False)
     extension = '.png'
     for index, image in enumerate(images):
         image = np.reshape(image, [28, 28])
-        plt.imshow(image, cmap='gray')
-        plt.savefig(os.path.join(images_dir, str(index)+extension))
+        # 注意把image的数据类型转化为uint8，并以灰度图模式('L')生成图片
+        image = Image.fromarray(np.uint8(image), 'L')
+        image.save(os.path.join(images_dir, str(index)+extension))
 
 
 if __name__ == '__main__':
--- inference.py
+++ inference.py
@@ -1,1 +1,34 @@
 # coding=utf-8
+from PIL import Image
+import sys
+import os
+import numpy as np
+import network
+from train import log_dir
+
+
+def inference():
+    images_dir = 'images/'
+    # 设置命令行参数更改图片目录
+    if len(sys.argv) == 2:
+        images_dir = sys.argv[1]
+
+    # 载入模型
+    model_name = 'model'
+    model_path = os.path.join(log_dir, model_name+'.json')
+    net = network.Network.load(model_path)
+
+    file_list = os.listdir(images_dir)
+    for file_path in file_list:
+        # 打开图片并转为灰度图
+        image = Image.open(os.path.join(images_dir, file_path)).convert('L')
+        image = image.resize((28, 28))
+        # 转化为numpy array并进行二值化处理
+        image = np.reshape(image, [784, 1])
+        image = np.clip(image, 0.0, 1.0)
+        prediction = np.argmax(net.predict(image))
+        print('%s: %s' % (file_path, prediction))
+
+
+if __name__ == '__main__':
+    inference()
