00b3bcf05eea7151fffac3c9c734588dc3a70bfe
Dimitri <791219648@qq.com>
2018-05-17 10:21:07 +0800

    add robustness

--- inference.py
+++ inference.py
@@ -12,14 +12,21 @@ def inference(net, images_dir):
     file_list = os.listdir(images_dir)
     print('Picture\t\t\tPrediction')
     for file_path in file_list:
-        # 打开图片并转为灰度图
-        image = Image.open(os.path.join(images_dir, file_path)).convert('L')
-        image = image.resize((28, 28))
-        # 转化为numpy array并进行二值化处理
-        image = np.reshape(image, [784, 1])
-        image = np.clip(image, 0.0, 1.0)
-        prediction = np.argmax(net.predict(image))
-        print('%s:\t\t\t%s' % (file_path, prediction))
+        # 只处理图片文件
+        extension = os.path.splitext(file_path)[1]
+        if extension not in ['.png', '.jpg', '.jpeg', '.bmp']:
+            continue
+        try:
+            # 打开图片并转为灰度图
+            image = Image.open(os.path.join(images_dir, file_path)).convert('L')
+            image = image.resize((28, 28))
+            # 转化为numpy array并进行二值化处理
+            image = np.reshape(image, [784, 1])
+            image = np.clip(image, 0.0, 1.0)
+            prediction = np.argmax(net.predict(image))
+            print('%s:\t\t\t%s' % (file_path, prediction))
+        except Exception as err:
+            print('%s:\t\t\tfail to predict: "%s"' % (file_path, err))
 
 
 def main():
